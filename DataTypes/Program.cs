300d4c77985ce7a8f1d5ee551911b9d1723e6e70
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-10-03 17:22:15 +0300

    Homework DONE

---
+++ DataTypes/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+        static readonly string delimeter = "\n|---------------------------------|\n";
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Hello DataTypes");
+            #region Bool
+            //Console.WriteLine(sizeof(bool));
+            //Console.WriteLine(true);
+            //Console.WriteLine(false);
+            //Console.WriteLine(Boolean.FalseString); 
+            #endregion
+            Console.WriteLine(sizeof(char));
+            Console.WriteLine('+'.GetType());
+
+            Console.WriteLine(delimeter);
+            Console.WriteLine("\t\tByte:");
+            Console.WriteLine("Size: " + sizeof(byte) + " Byte");
+            Console.WriteLine("MinValue: " + byte.MinValue);
+            Console.WriteLine("MaxValue: " + byte.MaxValue);
+            Console.WriteLine(delimeter);
+
+            Console.WriteLine("\t\tSbyte");
+            Console.WriteLine("Size: " + sizeof(sbyte) + " Byte");
+            Console.WriteLine("MinValue: " + sbyte.MinValue);
+            Console.WriteLine("MaxValue: " + sbyte.MaxValue);
+            Console.WriteLine(delimeter);
+
+            Console.WriteLine("\t\tfloat: ");
+            Console.WriteLine("Size: " + sizeof(float) + " byte");
+            Console.WriteLine("MinValue: " + float.MinValue);
+            Console.WriteLine("MaxValue: " + float.MaxValue);
+            Console.WriteLine(delimeter);
+
+            Console.WriteLine("\t\tdouble: ");
+            Console.WriteLine("Size: " + sizeof(double) + " byte");
+            Console.WriteLine("MinValue: " + double.MinValue);
+            Console.WriteLine("MaxValue: " + double.MaxValue);
+            Console.WriteLine(delimeter);
+
+            Console.WriteLine("\t\tDecimal: ");
+            Console.WriteLine("Size: " + sizeof(decimal) + " byte");
+            Console.WriteLine("MinValue: " + decimal.MinValue);
+            Console.WriteLine("MaxValue " + decimal.MaxValue);
+            Console.WriteLine(delimeter);
+
+            //Console.WriteLine(5ul.GetType());
+
+            Console.WriteLine(int.MaxValue);
+            int a = 2000000000;
+            int b = 4;
+            try
+            {
+                Console.WriteLine((a * b).GetType());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+             
+            }
+
+        }
+	}
+}
---
+++ HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿//#define FACTORIAL 
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+#if FACTORIAL
+			Console.WriteLine("Введите число: ");
+			int n = int.Parse(Console.ReadLine());
+			double f = 1;
+			Console.Write(n + " != ");
+			if (n == 0) Console.Write(1 + "\t");
+			else
+			{
+				for (int i = 1; i <= n; i++)
+				{
+					f *= i;
+				}
+				Console.Write(f + "\t");
+			} 
+#endif
+			int a = int.Parse(Console.ReadLine());
+			string symb = Console.ReadLine();
+			int b = int.Parse(Console.ReadLine());
+			switch (symb)
+			{
+				case"+": Console.WriteLine(a + " + " + b + " = " + (a + b));break;
+				case"-": Console.WriteLine(a + " - " + b + " = " + (a - b));break;
+				case"*": Console.WriteLine(a + " * " + b + " = " + (a * b));break;
+				case"/": Console.WriteLine(a + " / " + b + " = " + (a / b));break;
+			}
+
+		}
+	}
+}
