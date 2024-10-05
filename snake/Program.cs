8bb052b56c8d051d564b948636c025d7e8afbbd2
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-10-05 18:11:42 +0300

    Snake in progress

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,12 +10,53 @@ namespace snake
 	{
 		static void Main(string[] args)
 		{
-			Console.ReadKey();
+		const char UPPER_ARROW = (char)8593;
+            Console.WriteLine(UPPER_ARROW);
 			char key;
 			do
 			{
-				key = Console.ReadKey(true).KeyChar; 
-                Console.WriteLine((int)key + "\t" +key);
+				key = Console.ReadKey(true).KeyChar;
+                //Console.WriteLine((int)key);
+				try
+				{
+					switch (key)
+					{
+						case 'W':
+						case 'w':
+						case 'ц':
+						case 'Ц':
+							Console.CursorTop -= 1;
+							break;
+						case 'D':
+						case 'd':
+						case 'в':
+						case 'В':
+							Console.CursorLeft += 1;
+							break;
+						case 'A':
+						case 'a':
+						case 'ф':
+						case 'Ф':
+							Console.CursorLeft -= 1;
+							break;
+						case 'S':
+						case 's':
+						case 'ы':
+						case 'Ы':
+						case '↓':
+							Console.CursorTop += 1;
+							break;
+					}
+				}
+				catch (Exception ex)
+				{
+
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Press Enter for сontinue");
+					Console.ReadLine();
+					Console.Clear();
+				}
+				
 			} while (true);
 		}
 	}
